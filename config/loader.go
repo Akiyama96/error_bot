e8659f6e277d0ad761c970a150e9bf568baf5ca5
Akiyama96 <shiina96@163.com>
2023-02-21 00:31:36 +0800

    feat: service config setting

--- config/loader.go
+++ config/loader.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	"fmt"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gcfg"
 	"gopkg.in/yaml.v3"
 	"log"
 	"os"
@@ -28,6 +30,9 @@ var Content = new(content)
 
 // LoadConfig 加载配置文件
 func LoadConfig() {
+	g.Cfg().GetAdapter().(*gcfg.AdapterFile).SetPath(fmt.Sprintf("../"))
+	g.Cfg().GetAdapter().(*gcfg.AdapterFile).SetFileName("config.yaml")
+
 	configFile, err := os.ReadFile("../config.yaml")
 	if err != nil {
 		log.Fatal(fmt.Sprintf("ERROR: failed to load config, err(%s).", err))
